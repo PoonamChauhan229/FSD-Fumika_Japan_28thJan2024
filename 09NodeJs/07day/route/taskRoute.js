dbdb867ad3db7539262f1985ea4e6dff0cd82aef
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2024-08-24 17:15:36 +0530

    taskRoute

--- 09NodeJs/07day/route/taskRoute.js
+++ 09NodeJs/07day/route/taskRoute.js
@@ -13,17 +13,33 @@ router.post('/addtask',auth,async(req,res)=>{
         owner:req.user._id
     })
     await taskData.save()
-    res.send(taskData)
+    
+    res.send({taskData:taskData,message:"Task Created Successfully"})
+
+    //Object short hand property || key:value same so just pass keyname..rest will be hand;ed by js
+    // res.send({taskData,message:"Task Created Successfully"})
 })
 
 
 //GET REQUEST
-router.get('/task',async(req,res)=>{
-    const getAllTask=await Task.find({})
-    res.send(getAllTask)
+router.get('/task',auth,async(req,res)=>{
+//auth > token
+console.log(req.user._id)
+// user._id  >> 
+// virtual feild taskRel >> populated...
+//populate(virtalFeildName)
+await req.user.populate('taskRel')
+// in the beg TaskREl is empty (just had a relationship)
+// populate(add the data to it)
+// inside the taskRel
+// req.user
+//populate >>
+res.send({"TaskData":req.user.taskRel})
+
 })
 
 //GET_ ID
+// edit and update but with the specific data
 router.get('/task/:id',async(req,res)=>{
      try{
         const getTask=await Task.findById(req.params.id)
