567af173d20f49eea8158680b7a9b88d59a90e83
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2025-08-14 15:57:21 +0530

    completed fsd and project moviestation-14th Aug

--- 10PdfDownload/BE_PdfDownloader/index.js
+++ 10PdfDownload/BE_PdfDownloader/index.js
@@ -27,11 +27,15 @@ doc.end();
 
 })
 
+    //each cell >>header
 function drawTableRow(doc,y,row,widths){
+    console.log(row)
+
     let x=50;// x axis 
-    row.foreach((cell,index)=>{ // iterate over each cell
+    row.forEach((cell,i)=>{ // iterate over each cell
         // Print and execute here itself
         doc.text(cell,x,y,{width:widths[i],align:"left"}) //cell x y width alignment
+        x+=widths[i]
     })
 }
 
@@ -47,13 +51,39 @@ res.setHeader("content-Disposition","attachment ; filename=test.pdf")
 // Pipe PDF content Directly to the response
 doc.pipe(res)
 
-// Add the content to the PDF
-
 // clientname >>
-console.log("Fumika") //>> from the frontend
+console.log(req.query) //>> from the frontend
 
-//drawTableRow() //doc,y,row,widths >>???
+const invoice_Date=new Date().toLocaleDateString()
+const invoice_Id="6776778989"
 
+// Add the content to the PDF
+// doc.fontSize(25).text(`Invoice Generated for ${invoice_Id}, on ${invoice_Date}  Ordered By-${req.query.client}`, 100, 100);
+
+ doc.fontSize(25)
+ .text(`Invoice Generated for ${invoice_Id}`)
+ .text(`on ${invoice_Date}`)
+ .text(`Ordered By-${req.query.client}`)
+ .moveDown()
+
+// Table >> tableheaders + width
+const tableHeaders=["item","Qty","Price","Total"] //header row
+const colWidths=[200,100,100,100]  //width
+drawTableRow(doc,doc.y,tableHeaders,colWidths) //doc,y,row,widths >>???
+
+// sample data
+// const items=["abc","45","67",1000]
+const items=[
+    {name:"Design Work",qty:2,price:150},
+    {name:"Developement",qty:5,price:150},
+    {name:"Prototype UI",qty:7,price:150},
+    {name:"Deployment",qty:7,price:150},
+]
+
+items.forEach((element)=>{
+    const finalPrice=element.qty*element.price
+    drawTableRow(doc,doc.y,[element.name,element.qty,element.price,finalPrice],colWidths) 
+})
 
 
 // Finalize the PDF
--- 10PdfDownload/FE_PdfDownloader/src/Components/ClientDownloadInvoice.jsx
+++ 10PdfDownload/FE_PdfDownloader/src/Components/ClientDownloadInvoice.jsx
@@ -3,10 +3,47 @@ import { useState } from "react"
 export function ClientDownloadInvoice(){
     const [clientName,setclientName]=useState("")
 
-    const handleDownload=()=>{
-        console.log("Btn is presses",clientName)
-        // Other logics
+     const handleDownload=async()=>{
+      try{
+        // const response=await fetch(`http://localhost:8000/getInvoice?client=${encodeURIComponent(clientName)}`);
+         const response=await fetch(`http://localhost:8000/getInvoice?client=${clientName}`);
+         
+         console.log(`http://localhost:8000/getInvoice?client=${clientName}`)
+         console.log(``)
+              console.log(response)
+              if(!response.ok){ // false
+                throw new Error("Failed to download an PDF")
+              }
+              const blob=await response.blob() // object {}
+              const url=window.URL.createObjectURL(blob)
+              console.log(url)
+
+              // created a link as a child
+              const link=document.createElement("a")
+              link.href=url
+              // .download
+              link.download="order-summary-pdf"
+              // document.body.append(link)  // multiple at a time 
+              document.body.appendChild(link) // 1 at a time
+              link.click() //click and download
+
+              // remove a link as a child >> js vanilla
+              // removeChild
+              document.body.removeChild(link)
+
+
+              // remove >> ObjectURL >> Browser
+              window.URL.revokeObjectURL(url)
+              
+
+      }catch(e){
+        console.log("Download Error",e)
+      }
+
+
+
     }
+  
     return(
         <>
         <h1>Client Download Invoice</h1>
